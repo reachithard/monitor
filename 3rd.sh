a48c8ebb2a1d7e8bc2725bfc72592c087add3592
lzerone <lzerone@163.com>
2022-10-13 00:58:24 +0800

    deps  Please enter the commit message for your changes. Lines starting

--- 3rd.sh
+++ 3rd.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
-git submodule add git@github.com:the-tcpdump-group/libpcap.git ./thirdparty/libpcap
 git submodule add git@github.com:google/googletest.git ./thirdparty/gtest
 git submodule add git@github.com:gabime/spdlog.git ./thirdparty/spdlog
+git submodule add git@github.com:google/leveldb.git ./thirdparty/leveldb
+git submodule add git@github.com:ibireme/yyjson.git ./thirdparty/yyjson
+git submodule add git@github.com:chriskohlhoff/asio.git ./thirdparty/asio
+git submodule add git@github.com:protocolbuffers/protobuf.git ./thirdparty/protobuf
+git submodule add git@github.com:grpc/grpc.git ./thirdparty/grpc
